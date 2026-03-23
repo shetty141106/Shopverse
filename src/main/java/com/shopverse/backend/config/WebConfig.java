7948ab69d340034028ef1acd1a8995e90bc7c071
MOKSHITH SHETTY <mokshithshetty14@gmail.com>
2026-03-23 14:14:37 +0530

    Added CORS config

--- src/main/java/com/shopverse/backend/config/WebConfig.java
+++ src/main/java/com/shopverse/backend/config/WebConfig.java
@@ -1,14 +1,21 @@
-package com.shopverse.backend.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://comfy-rugelach-5333c5.netlify.app") // 🔥 your frontend URL
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
