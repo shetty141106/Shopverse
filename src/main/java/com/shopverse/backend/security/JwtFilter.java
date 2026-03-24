761c1677f2217d4b5e305e86f690ff55540fab61
MOKSHITH SHETTY <mokshithshetty14@gmail.com>
2026-03-24 12:28:50 +0530

    Final fix - build success

--- src/main/java/com/shopverse/backend/security/JwtFilter.java
+++ src/main/java/com/shopverse/backend/security/JwtFilter.java
@@ -12,7 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
-
+import io.jsonwebtoken.Claims;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
